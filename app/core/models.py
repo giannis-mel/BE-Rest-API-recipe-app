7e2f349d8990e625e09e4ec8db8ad269cb0aeacf
Giannis Meleziadis <meleziadisgiannis@gmail.com>
2023-12-16 10:48:46 +0200

    Added superuser test and functionality

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,8 @@ from django.contrib.auth.models import (
 )
 
 # AbstractBaseUser has the functionality for the authentication
+
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
@@ -18,6 +20,15 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
