4512ea0732da6e95127f9a2e6ea8631222da20b6
Giannis Meleziadis <meleziadisgiannis@gmail.com>
2023-12-27 18:19:37 +0200

    Added swagger functionality for API documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
     'core'
 ]
 
@@ -130,3 +132,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+# Set default schema class for OpenAPI documentation in Django REST Framework
+# using 'AutoSchema' from 'drf_spectacular'.
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
